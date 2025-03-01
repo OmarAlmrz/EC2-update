60a93207c5e23614a4d701f7085d26c0ce1b6950
OmarAlmrz <almaraz.omar@hotmail.com>
2025-03-01 17:13:36 +0100

    script to update db

---
+++ functions.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import gzip
+import json
+import logging
+import os
+
+def get_s3_json(key, bucket):
+    obj = bucket.Object(key).get()
+    try:
+        return pd.read_json(obj['Body'])
+    except ValueError:
+        return pd.read_json(obj['Body'], lines=True)
+
+def get_s3_gzip_json(key, bucket):
+    obj = bucket.Object(key).get()
+    return json.loads(gzip.decompress(obj['Body'].read()).decode('utf-8'))
+
+def init_logger(logger_name:str,  level=logging.INFO):
+    log_file = f"{logger_name}.log"
+   
+   
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+
+          
+    # Check if a FileHandler already exists by matching the absolute file path
+    if any(isinstance(h, logging.FileHandler) and os.path.basename(h.baseFilename) == f"{logger_name}.log" for h in logger.handlers):
+        return  logger
+            
+    file_handler = logging.FileHandler(log_file, encoding='utf-8')
+    logger.addHandler(file_handler)
+
+    # Set the formatting for the handler messages
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    return logger
---
+++ update.py
@@ -0,0 +1,67 @@
+# Description: This script is used to update the embeddings of db.
+import boto3
+import os
+import functions as f
+import chromadb
+
+# Load the environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# Initialize the logger
+logger = f.init_logger('update')
+
+# Initialize the S3 client
+s3 = boto3.resource(
+        service_name='s3',
+        aws_access_key_id= os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key= os.getenv('AWS_SECRET_ACCESS_KEY')	,
+    )
+
+bucket = s3.Bucket(os.getenv('S3_EMBEDDINGS'))
+
+# Path to report 
+# /mnt/data/vectordb/SCJN/Tesis/ - [Juris,Aislada ]
+# /mnt/data/vectordb/SCJN/Precedentes/ - [Precedentes]
+# /mnt/data/vectordb/Federal/ - [Leyes]
+
+## Change variables ##
+folder_path = "SCJN/Aislada/"
+database_path = "/mnt/data/vectordb/SCJN/Tesis/"
+collection_name = "Aislada"
+#######################
+
+# Load database
+client = chromadb.PersistentClient(path=database_path)
+if not client.heartbeat(): exit()
+
+# Load the collection
+collection = client.get_collection(collection_name)
+if not collection: exit()
+
+# Load the report
+report_path = f"{folder_path}report.json"
+df_report = f.get_s3_json(report_path, bucket)
+
+if df_report.empty: exit()
+
+for _, row in df_report.iterrows():
+    
+    # Path to the embeddings
+    embeddings_path = f"{folder_path}{row['file']}.gz"
+    
+    # Load compressed data 
+    compressed_data = f.get_s3_gzip_json(embeddings_path, bucket) 
+    
+    collection.add( 
+        documents=compressed_data['documents'],
+        metadatas=compressed_data['metadata'],
+        embeddings=compressed_data['embeddings'],
+        ids=compressed_data['ids']
+    )
+    
+    logger.info(f"Adding {row['file']} for {collection_name}")
+
+logger.info(f"Collection {collection_name} updated. Total elements: {collection.count()}")
+    
+    

c9192661791c581c6d86d997d6f23ab2dc7ceccd
OmarAlmrz <almaraz.omar@hotmail.com>
2026-01-12 04:21:15 +0000

    removing unused variable

--- update_scjn.py
+++ update_scjn.py
@@ -58,7 +58,6 @@ if __name__ == "__main__":
     upd = SCJNUpdater()
 
     for db in databases:
-        database_path = db['database_path']
         folder_path = db['folder_path']
         collection_name = db['collection_name']
         
